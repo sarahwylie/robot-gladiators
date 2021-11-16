619e358b23a11575784609d07688ac22812737ed
sarahwylie <sarahannemariawylie@gmail.com>
2021-11-16 11:41:00 -0600

    first blows!

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,11 +1,41 @@
+// Player info
 var playerName = window.prompt("What is your robot's name?");
-console.log(playerName);
-console.log("This logs a string, good for leaving yourself a message");
-console.log(10+10);
-console.log("Our robot's name is" + playerName);
+var playerHealth = 100;
+var playerAttack = 10;
+console.log(playerName, playerAttack, playerHealth);
 
-// this creates a function named "fight"
-function fight() {
-    window.alert("The fight has begun!");
+//enemy info
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+// create function
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+  };
+  // execute function
+  fight();
+
+// First player attack
+console.log(
+    playerName + "attacked" + enemyName + "." + enemyName + "now has" + enemyHealth + "health remaining."
+);
+// check enemy's health
+if (enemyHealth <= 0) {
+    window.alert(enemyName + "has died!");
 }
-// fight();
+else {
+    window.alert(enemyName + "still has" + enemyHealth + "health left.");
+};
+
+// First enemy attack
+console.log(
+    enemyName + "attacked" + playerName + "." + playerName + "now has" + playerHealth + "health remaining."
+);
+// check player's health
+if (playerHealth <= 0) {
+    window.alert(playerName + "has died!");
+}
+else {
+    window.alert(playerName + "still has" + playerHealth + "health left.");
+}
